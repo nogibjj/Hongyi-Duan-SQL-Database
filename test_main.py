94863c07a7fed083764c16eee18111b6f8902aa7
Lulu-1121 <hd162@duke.edu>
2024-10-07 00:57:11 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,6 +2,7 @@ import unittest
 import main
 
 class TestDatabaseOperations(unittest.TestCase):
+    """ Test cases for the CRUD operations in main.py """
 
     def setUp(self):
         """ Set up a new database before each test """
@@ -13,7 +14,7 @@ class TestDatabaseOperations(unittest.TestCase):
         self.conn.close()
 
     def test_insert_user(self):
-        """ Test inserting a user """
+        """ Test inserting a new user """
         main.insert_user(self.conn, "Test User", 25)
         users = main.get_users(self.conn)
         self.assertEqual(len(users), 1)
@@ -21,7 +22,7 @@ class TestDatabaseOperations(unittest.TestCase):
         self.assertEqual(users[0][2], 25)
 
     def test_update_user(self):
-        """ Test updating a user's age """
+        """ Test updating an existing user """
         main.insert_user(self.conn, "Test User", 25)
         main.update_user(self.conn, 1, 30)
         users = main.get_users(self.conn)
