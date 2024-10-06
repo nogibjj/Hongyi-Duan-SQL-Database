be231fa4864d3fbfb50b5f3bbd0d357df5c530c8
Lulu-1121 <hd162@duke.edu>
2024-10-07 00:39:27 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,38 @@
-from main import add
+import unittest
+import main
 
-def test_add():
-    assert add(1, 2) == 3
+class TestDatabaseOperations(unittest.TestCase):
+
+    def setUp(self):
+        """ Set up a new database before each test """
+        self.conn = main.connect_to_db(":memory:")
+        main.create_table(self.conn)
+
+    def tearDown(self):
+        """ Close the connection after each test """
+        self.conn.close()
+
+    def test_insert_user(self):
+        """ Test inserting a user """
+        main.insert_user(self.conn, "Test User", 25)
+        users = main.get_users(self.conn)
+        self.assertEqual(len(users), 1)
+        self.assertEqual(users[0][1], "Test User")
+        self.assertEqual(users[0][2], 25)
+
+    def test_update_user(self):
+        """ Test updating a user's age """
+        main.insert_user(self.conn, "Test User", 25)
+        main.update_user(self.conn, 1, 30)
+        users = main.get_users(self.conn)
+        self.assertEqual(users[0][2], 30)
+
+    def test_delete_user(self):
+        """ Test deleting a user """
+        main.insert_user(self.conn, "Test User", 25)
+        main.delete_user(self.conn, 1)
+        users = main.get_users(self.conn)
+        self.assertEqual(len(users), 0)
 
 if __name__ == "__main__":
-    test_add()
+    unittest.main()
